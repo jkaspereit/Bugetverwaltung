9f09aaefbf3fc453a4b3ee0d67c1771e0c96ebfd
Damian Puente <Damian.Puente@gmx.de>
2017-02-18 13:05:59 +0100

    Wesentliche Projektstruktur

--- Budgetverwaltung/BuVer_Web/src/main/java/de/lebk/budgetverwaltung/config/BuVerWebApplication.java
+++ Budgetverwaltung/BuVer_Web/src/main/java/de/lebk/budgetverwaltung/config/BuVerWebApplication.java
@@ -1,4 +1,4 @@
-package de.lebk;
+package de.lebk.budgetverwaltung.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
